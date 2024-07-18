1efde4c9e5604e269118ceb97a07240e6121bd42
RDvibe <91291838+RDvibe@users.noreply.github.com>
2024-07-18 12:08:22 -0500

    Update app_registros.py

--- app_registros.py
+++ app_registros.py
@@ -105,7 +105,7 @@ def mostrar_tiempo_trabajado(tiempo):
         return f"{seconds} segundos"
 
 # Interfaz de usuario de Streamlit
-st.title("Registro de Entradas y Salidas de Trabajadores")
+st.title("Registro de Entradas y Salidas de Trabajadores-Netsat srl (Apliación de prueba)")
 
 trabajador_nombre = st.text_input("Nombre del Trabajador")
 
